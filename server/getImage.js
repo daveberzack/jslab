b94b44644ef03b9eaf1f090f63701726d85df267
Dave Berzack <daveberzack@gmail.com>
2022-09-05 15:22:15 -0400

    initial commit

--- server/getImage.js
+++ server/getImage.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 module.exports = async (prompt) => {
   const replicate = new Replicate();
   const model = await replicate.models.get("stability-ai/stable-diffusion");
-  const prediction = await model.predict({ prompt: prompt });
+  const prediction = await model.predict({
+    prompt: prompt,
+    num_inference_steps: 200,
+    prompt_strength: 1,
+  });
   return prediction[0];
 };
